07df0131dc952d612fa460e199c5d2b469f59389
Vaishcodes14 <vnmugali199@gmail.com>
2025-08-13 18:52:51 +0530

    Update career_app.py

--- career_app.py
+++ career_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+df = pd.read_csv("career.csv")  # CSV in same repo folder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import tree
